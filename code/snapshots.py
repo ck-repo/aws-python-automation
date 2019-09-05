b95659aeb0a1ad40e9f705e402d606c5ba31b9c3
ckckcklab <ckilpatrick82@gmail.com>
2019-09-05 14:25:43 +0100

    with click module added

--- code/snapshots.py
+++ code/snapshots.py
@@ -1,6 +1,18 @@
 import boto3
+import click
 
 session = boto3.Session(profile_name="snapshots")
 ec2 = session.resource("ec2")
-for i in ec2.instances.all():
-    print(i)
+
+@click.command()
+def list_instances():
+    "List EC2 Instances"
+    for i in ec2.instances.all():
+        print(','.join((
+        i.id,
+        i.instance_type,
+        i.state["Name"])))
+
+    return
+
+list_instances()
