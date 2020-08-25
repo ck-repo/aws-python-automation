4cfbb6b481cfebf51673077797f110957d12f25c
ckckcklab <ckilpatrick82@gmail.com>
2020-08-25 17:51:55 +0100

    added second Lambda function to handle Rekognition job and integrated Rekognition with SNS

--- 03-videolyzer/videolyzer/handler.py
+++ 03-videolyzer/videolyzer/handler.py
@@ -1,5 +1,16 @@
 import urllib
 import boto3
+import os
+
+def start_processing_video(event, context):
+    for record in event['Records']:
+        start_label_detection(
+            record['s3']['bucket']['name'],
+            urllib.parse.unquote_plus(record['s3']['object']['key'])
+        )
+
+
+    return
 
 def start_label_detection(bucket, key):
     rekognition_client = boto3.client('rekognition')
@@ -9,19 +20,18 @@ def start_label_detection(bucket, key):
                 'Bucket': bucket,
                 'Name': key
             }
+        },
+        NotificationChannel={
+            'SNSTopicArn': os.environ['REKOGNITION_SNS_TOPIC_ARN'],
+            'RoleArn': os.environ['REKOGNITION_ROLE_ARN']
         })
 
-    print(response)
+    print('Here is your Job ID: ' + response['JobId'])
 
     return
 
+def handle_label_detection(event, context):
 
-def start_processing_video(event, context):
-    for record in event['Records']:
-        start_label_detection(
-            record['s3']['bucket']['name'],
-            urllib.parse.unquote_plus(record['s3']['object']['key'])
-        )
-
+    print(event)
 
     return
